64443b12aae4a9da9f83f813cf5de3093abdb3c8
franAndrad <franciscoezequielandrade@gmail.com>
2023-04-04 19:09:06 -0300

    se agregaron librerias para las rutas y para mostrar codigo en la pagina. Se administro tambien la estructura de los directorios

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Menu from './components/common/Menu';
+import Footer from './components/common/Footer';
+import Home from './components/views/Home';
+import Document from './components/views/Document';
+
 function App() {
   return (
-    <div className='container'>
-      <header >
-      </header>
-      <body>
-        <h1 className="display-4 text-center">Documentar Conocmimientos</h1>
-        <hr />
-      </body>
+    <div>
+      <BrowserRouter>
+        <Menu></Menu>
+        <Routes>
+          <Route extact path="/" element={<Home />} />
+          <Route extact path="/document" element={<Document />} />
+        </Routes>
+      </BrowserRouter>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/components/views/Document.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Document = () => {
+   return (
+      <div>
+         <p>Ver como administrar documentos</p>
+      </div>
+   );
+};
+
+export default Document;<p>Ver como administrar documentos</p>
