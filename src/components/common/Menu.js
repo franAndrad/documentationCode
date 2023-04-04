afc036fe8485a6fa6f85def0280a4ba971ca7a52
franAndrad <franciscoezequielandrade@gmail.com>
2023-04-04 19:35:40 -0300

    se agregaron los links para navegar en el navbar

--- src/components/common/Menu.js
+++ src/components/common/Menu.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Link } from 'react-router-dom';
 
 const Menu = () => {
    return (
-      <div>
-         <p>Menu</p>
-      </div>
+      <Navbar bg="light" expand="lg">
+         <Container>
+            <Navbar.Brand as={Link} end to='/' >DocNowledge</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+               <Nav className="me-auto">
+                  <Nav.Link as={Link} end to='/'>Home</Nav.Link>
+                  <Nav.Link as={Link} end to='/document'>Document</Nav.Link>
+               </Nav>
+            </Navbar.Collapse>
+         </Container>
+      </Navbar>
    );
 };
 
