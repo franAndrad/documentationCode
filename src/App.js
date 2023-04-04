c827eb550ece79a0ef9ab97bc149e7601a69c95e
franAndrad <franciscoezequielandrade@gmail.com>
2023-04-04 19:55:05 -0300

    se elimino el componente footer

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Menu from './components/common/Menu';
-import Footer from './components/common/Footer';
 import Home from './components/views/Home';
 import Document from './components/views/Document';
 
@@ -16,7 +15,6 @@ function App() {
           <Route extact path="/document" element={<Document />} />
         </Routes>
       </BrowserRouter>
-      <Footer></Footer>
     </div>
   );
 }
