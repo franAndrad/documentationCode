6d7d5d383fef0f8031e3b679c7a5bac5d8a006f7
franAndrad <franciscoezequielandrade@gmail.com>
2023-04-04 19:50:09 -0300

    se cambio el color de la barra de navegacion a dark y se cambio el titulo del proyecto

--- src/components/common/Menu.js
+++ src/components/common/Menu.js
@@ -2,12 +2,11 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import { Link } from 'react-router-dom';
 
 const Menu = () => {
    return (
-      <Navbar bg="light" expand="lg">
+      <Navbar bg="dark" variant='dark' expand="lg">
          <Container>
             <Navbar.Brand as={Link} end to='/' >DocNowledge</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
