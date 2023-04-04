e211f02c8a3c9158abb42c229fc9a5a0a686b2e9
franAndrad <franciscoezequielandrade@gmail.com>
2023-04-04 14:39:24 -0300

    se creo la estructura del proyecto

---
+++ src/components/Titulo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Titulo = () => {
+   return (
+      <div>
+         <h1>Ejemplo Componente</h1>
+         <hr />
+      </div>
+   );
+};
+
+export default Titulo;
