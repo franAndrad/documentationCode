0038d4392a7f2b15e9ae0510affc72a429a983ca
franAndrad <franciscoezequielandrade@gmail.com>
2023-04-06 17:14:42 -0300

    se agrego para poder crear un subtitulo o seleccionar alguno

--- src/components/views/Document.js
+++ src/components/views/Document.js
@@ -8,12 +8,15 @@ import Options from './Select/OptionSub';
 import TituloSelect from './Select/TituloSelect';
 
 import './Document.css';
+import { set } from 'lodash';
 
 const Document = () => {
    const [temas , setTemas] = useState({});
    const [text, setText] = useState('');
    const [code, setCode] = useState('');
    const [editar, setEditar] = useState(false);
+   const [subtitulo, setSubtitulo] = useState('')
+   const [stateSelect, setStateSelect] = useState(true)
 
    const [idT, setIdT] = useState(0);
    const [idS, setIdS] = useState(0);
@@ -35,11 +38,48 @@ const Document = () => {
    // MODIFICAR!!!!! EL temmas.titulo[0] para que se modifique el titulo a escribir, analizar el mejor metodo para seleccionar titulos
    const handleSubmit = async (e) => {
       e.preventDefault();
-      if((text !== '') || (code !== '')){
+
+
+      if(subtitulo !== ''){
+         let id;
+         if (temas.titulo[0].subtitulo.length === 0) {
+            id = 1;
+         } else {
+            id = temas.titulo[0].subtitulo[temas.titulo[0].subtitulo.length - 1].id + 1
+         }
+         const nuevoSubtitulo = {
+            id: id,
+            nombre: subtitulo,
+            parrafos:[]
+         };
+         const nuevoObjeto = { ...temas };
+         nuevoObjeto.titulo[0].subtitulo.push(nuevoSubtitulo);
+         console.log(nuevoObjeto)
+         try {
+            const respuesta = await fetch("http://localhost:4000/tema/", {
+               method: "PUT",
+               headers: { "Content-Type": "application/json", },
+               body: JSON.stringify(nuevoObjeto),
+            });
+            setSubtitulo('');
+            setText('');
+            setCode('');
+            setStateSelect(true)
+            consultarTemas();
+         } catch (error) {
+            console.log(error);
+         }
+      }else if((text !== '') || (code !== '')){
+         let id;
+         if (temas.titulo[0].subtitulo[idS - 1].parrafos.length === 0){
+            id = 1;
+         } else {
+            id = temas.titulo[0].subtitulo[idS - 1].parrafos[temas.titulo[0].subtitulo[idS - 1].parrafos.length - 1].id + 1
+         }
          const nuevoParrafo = {
             linea: text,
             code: code,
-            id: temas.titulo[0].subtitulo[idS - 1].parrafos[temas.titulo[0].subtitulo[idS - 1].parrafos.length - 1].id + 1
+            id: id
          };
          const nuevoObjeto = {...temas};
          nuevoObjeto.titulo[0].subtitulo[idS - 1].parrafos.push(nuevoParrafo);
@@ -55,9 +95,6 @@ const Document = () => {
          } catch (error) {
             console.log(error);
          }
-      }
-      else{
-         console.log('enviar alerta')
       }
    }
 
@@ -125,14 +162,18 @@ const Document = () => {
             {temas.titulo === undefined ? '' : (temas.titulo.map((titulo) => (<Titulos titulo={titulo} key={titulo.id} idTitulo={titulo.id} consultarTemas={consultarTemas} handleDelete={handleDelete} handleUpdate={handleUpdate}/>)))}
             <Form onSubmit={editar!== true ? handleSubmit : handleUpdateParams} className='my-5 row p-3 rounded border border-dark'>
                <h5 className='text-center' id="form">Ingrese lo que desea agregar</h5>
-               {temas.titulo === undefined ? '' : (temas.titulo.map((titulo) => (<TituloSelect titulo={titulo} key={titulo.id} idTitulo={titulo.id} consultarTemas={consultarTemas} updateIdSub={updateIdSub} idS={idS} />)))} 
-                  {/* <input type='text' value={text} rows={3} className="background-form border border-dark text-light" placeholder='Ingrese el parrafo' onChange={(e) => setText(e.target.value)}></input> */}
-               <Form.Group className="my-3" controlId="text">
-                  <Form.Control type='text' value={text} rows={3} className="background-form border border-dark text-light" placeholder='Ingrese el parrafo' onChange={(e) => setText(e.target.value)}></Form.Control>
-               </Form.Group>
-               <Form.Group className="my-3" controlId="code">
-                  <CodeEditor setCode={setCode} code={code}></CodeEditor>
-               </Form.Group>
+               {(temas.titulo === undefined) ? '' : (temas.titulo.map((titulo) => (<TituloSelect titulo={titulo} key={titulo.id} idTitulo={titulo.id} consultarTemas={consultarTemas} updateIdSub={updateIdSub} idS={idS} stateEditar={editar} setSubtitulo={setSubtitulo} subtitulo={subtitulo} setStateSelect={setStateSelect} stateSelect={stateSelect} setCode={setCode} setText={setText}/>)))} 
+               {stateSelect !== false ?
+                  <Form.Group className="my-3" controlId="text">
+                     <Form.Control type='text' value={text} rows={3}  className="background-form border border-dark text-light" placeholder='Ingrese el parrafo' onChange={(e) => setText(e.target.value)}></Form.Control>
+                  </Form.Group>: ''
+               }
+               {stateSelect !== false ?
+                  <Form.Group className="my-3" controlId="code">
+                     <CodeEditor setCode={setCode} code={code}></CodeEditor>
+                  </Form.Group>: ''
+               }
+               
                <div className='text-center mb-3 mt-2'>
                   <Button type='submit' variant='dark' className='w-25'>{editar!== true ? "Enviar" : "Editar"}</Button>
                </div>
